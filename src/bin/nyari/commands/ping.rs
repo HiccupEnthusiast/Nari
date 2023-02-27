79054d77ef312136201cad42e87faaa9a50374ba
Aririi <37724510+Aririi@users.noreply.github.com>
2023-02-26 19:37:50 -0800

    changed to serenity

---
+++ src/bin/nyari/commands/ping.rs
@@ -0,0 +1,13 @@
+use serenity::{
+    framework::standard::{macros::command, CommandResult},
+    model::prelude::Message,
+    prelude::Context,
+};
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id
+        .send_message(&ctx, |m| m.content("Pong!"))
+        .await?;
+    Ok(())
+}
--- src/bin/nyari/main.rs
+++ src/bin/nyari/main.rs
@@ -1,67 +1,52 @@
-use std::{env, sync::Arc};
+pub mod commands;
 
-use twilight_cache_inmemory::{InMemoryCache, ResourceType};
-use twilight_gateway::{Event, Shard, ShardId};
-use twilight_http::Client as HttpClient;
-use twilight_model::gateway::Intents;
+use std::env;
 
-#[tokio::main]
-async fn main() {
-    dotenv::dotenv().expect("Failed to load .env file");
-    tracing_subscriber::fmt::init();
+use serenity::{
+    async_trait,
+    framework::{standard::macros::group, StandardFramework},
+    http::Http,
+    model::prelude::Ready,
+    prelude::{Context, EventHandler, GatewayIntents},
+    Client,
+};
 
-    let token = env::var("DISCORD_TOKEN").expect("Expected DISCORD_TOKEN in environment variables");
+use crate::commands::hello::HELLO_COMMAND;
+use crate::commands::ping::PING_COMMAND;
 
-    // Use intents to only receive guild message events.
-    let mut shard = Shard::new(
-        ShardId::ONE,
-        token.clone(),
-        Intents::GUILD_MESSAGES | Intents::MESSAGE_CONTENT,
-    );
+#[group]
+#[commands(ping, hello)]
+struct General;
+struct Handler;
 
-    // HTTP is separate from the gateway, so create a new client.
-    let http = Arc::new(HttpClient::new(token));
-
-    // Since we only care about new messages, make the cache only
-    // cache new messages.
-    let cache = InMemoryCache::builder()
-        .resource_types(ResourceType::MESSAGE)
-        .build();
-
-    // Process each event as they come in.
-    loop {
-        let event = match shard.next_event().await {
-            Ok(event) => event,
-            Err(source) => {
-                println!("error receiving event {source}");
-
-                if source.is_fatal() {
-                    break;
-                }
-
-                continue;
-            }
-        };
-
-        // Update the cache with the event.
-        cache.update(&event);
-
-        tokio::spawn(handle_event(event, Arc::clone(&http)));
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, r: Ready) {
+        println!("Connected as: {}#{}", r.user.name, r.user.discriminator)
     }
 }
 
-async fn handle_event(
-    event: Event,
-    http: Arc<HttpClient>,
-) {
-    match event {
-        Event::Ready(r) => {
-            println!("Connected as: {}#{}", r.user.name, r.user.discriminator)
-        }
-        Event::MessageCreate(msg) => {
-            println!("{}: {}", msg.author.name, msg.content)
-        }
-        // Other events here...
-        _ => {}
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    dotenv::dotenv().expect("Failed to load .env file");
+    let token = env::var("DISCORD_TOKEN").expect("Expected DISCORD_TOKEN in environment variables");
+
+    let http = Http::new(&token);
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("n!"))
+        .group(&GENERAL_GROUP);
+
+    let intents = GatewayIntents::all();
+
+    let mut bot = Client::builder(&token, intents)
+        .framework(framework)
+        .event_handler(Handler)
+        .await
+        .expect("Error while creating the bot");
+
+    if let Err(e) = bot.start().await {
+        println!("Error while starting the bot: {:?}", e);
     }
 }
