1ea98df31bc272da119d2c3bfee16059d1f95450
Hicster Enthusiast <hicster.enthusiast@gmail.com>
2023-03-21 20:14:21 -0500

    feat(crate, database): Add documentation

--- nari/src/lib.rs
+++ nari/src/lib.rs
@@ -1,1 +1,33 @@
+//! Nari is a crate aimed to provide different productivity tools to your application.
+//!
+//! It is built with consistency between runs in mind, to achieve this it saves most
+//! of its information in the filesystem, different approaches like using NoSQL/SQL
+//! databases or an entirely in-memory approach may come in the future.
+//!   
+//! [`Event`] represents any possible event that can happen. It provides any possible
+//! important information that a event can have, check its documentation for further
+//! information.
+//!
+//! [`Database`] represents a database, it provides functions to interact with it and
+//! it lays mostly inside files. These files althought human readable([`.ron`]), are not supposed to
+//! be interacted outside of nari. We may change the file specification or make nari work
+//! better if these files are modified externally in the future.
+//!
+//! [`EventListener`] provides an easy way to create a connection using tokio channels to
+//! future events. It sends events through a mspc channel whenever their unix timestamp is
+//! reached, how often this condition is checked  
+//!
+//! To see it in action you can look at [`examples`] to get a quick grasp on how to get running with nari.
+//!
+//! If you would rather have a fully fledged application ready, you can check our [`github repo`]
+//! for our projects built from nari.
+//!
+//! [`Event`]: crate::models::event::Event
+//! [`Database`]: crate::models::Database
+//! [`EventListener`]: crate::models::event::EventListener
+//! [`.ron`]: https://github.com/ron-rs/ron
+//! [`github repo`]: https://github.com/HiccupEnthusiast/Nari
+//! [`examples`]: https://github.com/HiccupEnthusiast/Nari/nari/examples
+
+/// This module holds the structure of nari.
 pub mod models;
--- nari/src/models/database.rs
+++ nari/src/models/database.rs
@@ -8,10 +8,24 @@ use std::{
     path::{Path, PathBuf},
 };
 
+/// Main interface to interact with the internal files
 pub struct Database {
     base_path: PathBuf,
 }
 impl Database {
+    /// Creates a new database representation, if using a filesystem schema,
+    /// it accepts the relative path where the file and folders will be created,
+    /// does not create a new folder to contain the rest of the database.
+    ///
+    /// ### Usage
+    /// ```no_run
+    /// # use nari::models::Database;
+    /// # fn main() -> std::io::Result<()> {
+    /// // Creates the database files and saves its contents in "db" //
+    /// let db = Database::new("./db")?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn new<P>(base_path: P) -> io::Result<Self>
     where
         P: AsRef<Path>,
@@ -32,23 +46,101 @@ impl Database {
 
         Ok(Self { base_path })
     }
+    /// Creates and adds an user to the database, returns the created user.
+    ///
+    /// ## Usage
+    /// ```no_run
+    /// # use nari::models::Database;
+    /// # use nari::models::UserId;
+    /// # fn main() {
+    /// # let db = Database::new("./db/").unwrap();
+    /// let alice = db.create_user(UserId(42), "Alice");
+    /// # }
+    /// ```
     pub fn create_user(&self, id: UserId, name: &str) -> User {
         let user = User::new(id, name);
         self.add_user(user.clone());
         user
     }
+    /// Adds an already created user to the database, consumes the user.
+    ///
+    /// ## Usage
+    /// ```no_run
+    /// # use nari::models::Database;
+    /// # use nari::models::UserId;
+    /// # use nari::models::User;
+    /// # fn main() {
+    /// # let db = Database::new("./db/").unwrap();
+    /// let alice = User::new(UserId(42), "Alice");
+    /// db.add_user(alice);
+    /// # }
+    /// ```
     pub fn add_user(&self, user: User) {
         let buf = self.open_buf_writer(user.id.0, "users").unwrap();
         ron::ser::to_writer(buf, &user).unwrap();
     }
+    /// Search for a user in the database, returns the user if found.
+    ///
+    /// ## Usage
+    /// ```no_run
+    /// # use nari::models::Database;
+    /// use nari::models::UserId;
+    /// # fn main() {
+    /// # let db = Database::new("./db/").unwrap();
+    /// let alice = db.create_user(UserId(42), "Alice");
+    /// assert_eq!(alice, db.fetch_user(UserId(42)));
+    /// # }
+    /// ```
     pub fn fetch_user(&self, id: UserId) -> User {
         let buf = self.open_buf_reader(id.0, "users").unwrap();
         ron::de::from_reader(buf).unwrap()
     }
 
+    /// Returns an [`EventBuilder`], with the minimum information required.
+    ///
+    /// Takes an [`EventId`] which must represent an unique u64 value, the name for the event
+    /// and a u64 number representing an unix timestamp of when should it fire.
+    ///  
+    /// It doesn't add the event to the database until it is built and manually
+    /// added.
+    ///
+    /// ## Usage
+    /// ```no_run
+    /// # use nari::models::Database;
+    /// # use nari::models::EventId;
+    /// # fn main() {
+    /// # let db = Database::new("./db/").unwrap();
+    /// // We build a new event for Alice's birthday and manually add it to the database
+    /// let birthday = db.build_event(EventId(14), "Alice's Birthday", 123456789)
+    ///         .build()
+    ///         .save_to_db(&db);
+    /// # }
+    /// ```
     pub fn build_event(&self, id: EventId, name: &str, next_occurence: u64) -> EventBuilder {
         EventBuilder::new(id, name, next_occurence)
     }
+    /// Adds an already created event to the database, consumes the event.
+    ///
+    /// It adds it to the database automatically.
+    ///
+    /// ## Usage
+    /// ```no_run
+    /// # use nari::models::Database;
+    /// # use nari::models::UserId;
+    /// # use nari::models::EventId;
+    /// # use nari::models::User;
+    /// # use nari::models::event::EventBuilder;
+    /// # fn main() {
+    /// # let db = Database::new("./db/").unwrap();
+    /// let alice = User::new(UserId(42), "Alice");
+    /// let bob = User::new(UserId(43), "Bob");
+    /// let alices_birthday = EventBuilder::new(EventId(42), "Alice's Birthday", 123456789)
+    ///         .description("Today is Alice's birthday! 🎉")
+    ///         .users([alice.id, bob.id])
+    ///         .build();
+    /// db.add_event(alices_birthday);
+    /// # }
+    /// ```
     pub fn add_event(&self, event: Event) {
         self.add_event_to_cache(&event);
         let buf = self.open_buf_writer(event.id.0, "events").unwrap();
@@ -62,11 +154,52 @@ impl Database {
             self.add_event_to_users(event, users)
         }
     }
+    /// Search for a event in the database, returns the event if found.
+    ///
+    /// ## Usage
+    /// ```no_run
+    /// # use nari::models::Database;
+    /// # use nari::models::EventId;
+    /// # use nari::models::event::EventBuilder;
+    /// # fn main() {
+    /// # let db = Database::new("./db/").unwrap();
+    /// let alices_birthday = EventBuilder::new(EventId(10), "Alice's Birthday", 123456789)
+    ///         .build();
+    /// db.add_event(alices_birthday.clone());
+    /// assert_eq!(alices_birthday, db.fetch_event(EventId(10)));
+    /// # }
+    /// ```
     pub fn fetch_event(&self, id: EventId) -> Event {
         let buf = self.open_buf_reader(id.0, "events").unwrap();
         ron::de::from_reader(buf).unwrap()
     }
 
+    /// Takes an event and adds it to any amount of users, it can take any
+    /// collection of [`User`] as long as it implements the [`IntoIterator`] trait.
+    ///
+    /// It saves the event and the users to the database automatically.
+    ///
+    /// [`User`] tracks what events it is in with a [`HashSet`](std::collections::HashSet) internally. This means
+    /// that if any two or more events have the same [`EventId`], they won't repeat
+    /// and only the latest one created will be used.
+    ///   
+    /// ## Usage
+    /// ```no_run
+    /// # use nari::models::Database;
+    /// # use nari::models::event::Event;
+    /// # use nari::models::{EventId, UserId};
+    /// # use nari::models::User;
+    /// # use nari::models::event::EventBuilder;
+    /// # fn main() {
+    /// # let db = Database::new("./db/").unwrap();
+    /// # let alice = User::new(UserId(42), "Alice");
+    /// # let bob = User::new(UserId(43), "Bob");
+    /// let alices_birthday = EventBuilder::new(EventId(42), "Alice's Birthday", 123456789)
+    ///         .description("Today is Alice's birthday! 🎉")
+    ///         .build();
+    /// db.add_event_to_users(alices_birthday, [alice, bob]);
+    /// # }
+    /// ```
     pub fn add_event_to_users<I>(&self, mut event: Event, users: I)
     where
         I: IntoIterator<Item = User>,
@@ -83,6 +216,35 @@ impl Database {
         let buf = self.open_buf_writer(event.id.0, "events").unwrap();
         ron::ser::to_writer(buf, &event).unwrap()
     }
+    /// Takes an user and adds it to any amount of events, it can take any
+    /// collection of [`Event`] as long as it implements the [`IntoIterator`] trait.
+    ///
+    /// It saves the user and the events to the database automatically.
+    ///
+    /// [`Event`] tracks what users it has with a [`HashSet`](std::collections::HashSet) internally. This means
+    /// that if any two or more users have the same [`UserId`], they won't repeat
+    /// and only the lastest one created will be used.
+    ///
+    /// ## Usage
+    /// ```no_run
+    /// # use nari::models::Database;
+    /// # use nari::models::event::Event;
+    /// # use nari::models::UserId;
+    /// # use nari::models::event::EventBuilder;
+    /// # use nari::models::EventId;
+    /// # use nari::models::User;
+    /// # fn main() {
+    /// # let db = Database::new("./db/").unwrap();
+    /// let bob = User::new(UserId(43), "Bob");
+    /// let alices_birthday = EventBuilder::new(EventId(42), "Alice's Birthday", 123456789)
+    ///        .build();
+    /// let job_meeting = EventBuilder::new(EventId(43), "A job meeting", 123456789)
+    ///       .build();  
+    /// let park_hangout = EventBuilder::new(EventId(44), "Park hangout", 123456789)
+    ///       .build();   
+    /// db.add_user_to_events(bob, [alices_birthday, job_meeting, park_hangout])
+    /// # }
+    /// ```
     pub fn add_user_to_events<I>(&self, mut user: User, events: I)
     where
         I: IntoIterator<Item = Event>,
@@ -98,6 +260,8 @@ impl Database {
         let buf = self.open_buf_writer(user.id.0, "users").unwrap();
         ron::ser::to_writer(buf, &user).unwrap();
     }
+    /// Reads the whole database and replaces the current event queue of future events
+    /// with the one read. It should fix any possible desync problems that may have arisen.
     pub fn rewrite_cache(&self) {
         let dir = std::fs::read_dir(self.base_path.join("events")).unwrap();
         let mut events = BTreeMap::new();
